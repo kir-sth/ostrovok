a8724bdf398563019a2e9824d0204b3ffefe6609
Kir <kir-sth@yandex.ru>
2024-06-01 19:28:00 +0300

    add pydantic schemas

--- app/main.py
+++ app/main.py
@@ -1,12 +1,31 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from schemas import HotelsSearchArgs, HotelSchema, BookingSchema
 
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
+
+@app.get("/hotels")
+async def get_hotels(
+    hotels_search_args: HotelsSearchArgs = Depends(),
+) -> list[HotelSchema]:
+    hotels = [
+        {
+            "address": "Ул. Гагарина, 1",
+            "name": "Super Hotel",
+            "stars": 5
+        }
+    ]
+    return hotels
+
+
+@app.post("/booking")
+async def add_booking(
+    booking: BookingSchema
+):
+    pass
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ app/schemas.py
@@ -0,0 +1,32 @@
+from fastapi import  Query
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+
+class HotelSchema(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+class HotelsSearchArgs():
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(default=None, ge=1, le=5),
+        has_spa: Optional[bool] = None,
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
+
+
+class BookingSchema(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
